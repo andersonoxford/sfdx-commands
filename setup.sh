5ec9bd2889a2bdfdad5f9f25f1a06e8d0a322141
Harout Tatarian <harouttatarian@hotmail.com>
2020-11-04 08:25:55 -0500

    Refactoring

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo 'UPDATED Commands 2'
+echo 'UPDATED Commandsss'
 GITHUB_URL='https://raw.githubusercontent.com/andersonoxford/sfdx-commands/master/'
 PREFIX="bash <(curl -s ${GITHUB_URL}"
 SUFFIX=")"
