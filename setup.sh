88a119e3cd0e72d00bc2e8f4478c825f00c32e46
Harout Tatarian <harouttatarian@hotmail.com>
2020-11-04 08:23:39 -0500

    Refactoring

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-echo 'UPDATED Commands'
+echo 'UPDATED Commands 2'
 GITHUB_URL='https://raw.githubusercontent.com/andersonoxford/sfdx-commands/master/'
 PREFIX="bash <(curl -s ${GITHUB_URL}"
 SUFFIX=")"
+echo "${PREFIX}source_push.sh${SUFFIX}"
 alias push='${PREFIX}source_push.sh${SUFFIX}'
